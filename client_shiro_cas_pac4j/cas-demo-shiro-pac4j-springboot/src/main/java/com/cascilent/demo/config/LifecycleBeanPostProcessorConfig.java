41879bc6453b6e92a4f698216a4fe9d0b84a79ac
2356899074@qq.com <2356899074@qq.com>
2017-12-13 11:39:01 +0800

    优化及处理LifecycleBeanPostProcessor

--- client_shiro_cas_pac4j/cas-demo-shiro-pac4j-springboot/src/main/java/com/cascilent/demo/config/LifecycleBeanPostProcessorConfig.java
+++ client_shiro_cas_pac4j/cas-demo-shiro-pac4j-springboot/src/main/java/com/cascilent/demo/config/LifecycleBeanPostProcessorConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 /**
- * Shiro生命周期处理器,开启了就会启动报错不知道为什么？---设置为优先启动！！
+ * Shiro生命周期处理器,开启了就会启动报错不知道为什么？---设置为优先启动！！见：LifecycleBeanPostProcessorConfig
  * @Description:TODO
  * @author:hsj qq:2356899074
  * @time:2017年12月13日 上午11:34:49
